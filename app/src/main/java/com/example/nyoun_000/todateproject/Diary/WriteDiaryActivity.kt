42c601b7125193f551a64e99d415852d9b509702
USER-PC\USER <nyounh@gmail.com>
2018-03-02 21:12:30 +0900

    firebase 연동

--- app/src/main/java/com/example/nyoun_000/todateproject/Diary/WriteDiaryActivity.kt
+++ app/src/main/java/com/example/nyoun_000/todateproject/Diary/WriteDiaryActivity.kt
@@ -14,27 +14,51 @@ import com.example.nyoun_000.todateproject.DB.DBManagerDiary
 import com.example.nyoun_000.todateproject.DB.DiaryData
 import com.example.nyoun_000.todateproject.Manifest
 import com.example.nyoun_000.todateproject.R
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_write_diary.*
+import org.jetbrains.anko.progressDialog
+import org.jetbrains.anko.toast
 import java.io.BufferedOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.net.URI
+import java.text.SimpleDateFormat
+import java.util.logging.SimpleFormatter
+import com.google.firebase.auth.FirebaseUser
+
+
 
 class WriteDiaryActivity : AppCompatActivity() {
+    //lateinit var  filePath : String
+    lateinit var uploadFile : Uri
+    lateinit var mAuth : FirebaseAuth
+
     companion object {
         const val IMAGE_FROM_ALBUM = 1
         const val IMAGE_FROM_CROP = 2
+        //val mStorageRef = FirebaseStorage.getInstance().reference
     }
     init {
         DBManagerDiary.init(this)
-
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write_diary)
-        val mStorageReference = FirebaseStorage.getInstance().reference
+        //임시 익명 로그인
+        mAuth = FirebaseAuth.getInstance()
+        mAuth.signInAnonymously()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful){
+                        toast("익명 인증 성공!!!")
+                    } else {
+                        toast("익명 인증 실패...")
+                    }
+                }
+        //
+
 
         var intent = intent
         var selectedYear = intent.extras.getString("selectedYear")
@@ -59,14 +83,38 @@ class WriteDiaryActivity : AppCompatActivity() {
                     selectedMonth,
                     selectedDay)
             DBManagerDiary.addDiaryData(data)
+            uploadFile(selectedYear+"-"+selectedMonth+"-"+selectedDay)
             finish()
         }
         bt_cancel.setOnClickListener {
             finish()
         }
     }
-    fun doTakeAlbumAction(){
+    fun uploadFile(date : String){
+        var uploadDialog = progressDialog("업로드 중...", "uploading data")
+        uploadDialog.show()
+        if(uploadFile!=null){
 
+            val mStorage = FirebaseStorage.getInstance()
+            val fileName = date + ".jpg"
+
+            val storageRef : StorageReference = mStorage.getReferenceFromUrl("gs://todate-1dec6.appspot.com").child("images/" + fileName)
+            storageRef.putFile(uploadFile).addOnSuccessListener {
+                uploadDialog.dismiss()
+                toast("업로드 성공!!!")
+            }.addOnFailureListener {
+                uploadDialog.dismiss()
+                toast("업로드 실패...")
+            }.addOnProgressListener {
+                        @SuppressWarnings("VisibleForTests")
+                        val progress = 100 * it.bytesTransferred / it.totalByteCount
+                        //dialog에 진행률을 퍼센트로 출력해 준다
+                        uploadDialog.setMessage("Uploaded " + progress.toInt() + "% ...")
+            }
+        } else {
+            toast("파일이 선택되지 않음!!!")
+        }
+        uploadDialog.dismiss()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -78,6 +126,10 @@ class WriteDiaryActivity : AppCompatActivity() {
 
         if (requestCode == IMAGE_FROM_ALBUM){
             val uri : Uri = data!!.data
+            //
+            uploadFile = data!!.data
+
+            //
             val intent = Intent("com.android.camera.action.CROP")
             intent.setDataAndType(uri, "image/*")
             intent.putExtra("outputX", 200) // CROP한 이미지의 x축 크기
@@ -109,9 +161,15 @@ class WriteDiaryActivity : AppCompatActivity() {
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+
+    }
+
     override fun onStop() {
         super.onStop()
         DBManagerDiary.db_close()
+
     }
     fun storeCropImage(bitmap : Bitmap, filePath : String) {
        //폴더를 생성하여 이미지를 저장하는 방식이다.
